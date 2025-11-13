89318465394f0be52347005a334cc030fd64a397
Truong Cong Thien Phu <truongcongthienphu1910.work@gmail.com>
2025-11-13 21:06:05 +0700

    chore(SlangWordFileProviderclass): test SlangWordFileProvider's methods on Main class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+import java.util.HashMap;
+
+import provider.SlangWordFileProvider;
+
 public class Main {
-  
+  public static void main(String[] args) {
+    String filePath = "./input/slang.txt";
+    SlangWordFileProvider provider = new SlangWordFileProvider(filePath);
+    HashMap<String, String> slangwords = provider.importSlangWord();
+
+    //Set<String> set = slangwords.keySet();
+    System.out.println(slangwords.size());
+  }
 }
