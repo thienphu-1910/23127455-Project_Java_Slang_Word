940dfd88a6bd091e7e36368829dcab1b0fe94d03
Truong Cong Thien Phu <truongcongthienphu1910.work@gmail.com>
2025-11-15 00:12:01 +0700

    chore: create DataContainer and SearchingContainer classes for storing data

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,9 +1,22 @@
 package controller;
+
 import command.Command;
+import models.DataContainer;
+import models.SearchingContainer;
 
 public class Controller {
+  private DataContainer dataContainer = new DataContainer();
+  private SearchingContainer searchingContainer = new SearchingContainer();
 
-  
+  public Controller() {}
+
+  public DataContainer getDataContainer() {
+    return new DataContainer(this.dataContainer);
+  }
+  public SearchingContainer getSearchingContainer() {
+    return new SearchingContainer(this.searchingContainer);
+  }
+
   public void init() {
 
   }
