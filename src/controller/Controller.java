2488c5053b7960cd771c2dfa5754f69e365cd476
Truong Cong Thien Phu <truongcongthienphu1910.work@gmail.com>
2025-11-15 15:07:07 +0700

    refactor: replace AddSlangData to SlangData

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -3,18 +3,30 @@ package controller;
 import command.Command;
 import models.Glossary;
 import models.SearchSlangData;
+import models.SlangData;
 
 public class Controller {
-  private Glossary dataContainer = new Glossary();
-  private SearchSlangData searchingContainer = new SearchSlangData();
+  private Glossary glossary = new Glossary();
+  private SearchSlangData searchSlangData = new SearchSlangData();
+  private SlangData addSlangData = new SlangData();
+  private SlangData deleteSlangData = new SlangData();
 
   public Controller() {}
 
   public Glossary getGlossary() {
-    return new Glossary(this.dataContainer);
+    return new Glossary(this.glossary);
   }
   public SearchSlangData getSearchSlangData() {
-    return new SearchSlangData(this.searchingContainer);
+    return new SearchSlangData(this.searchSlangData);
+  }
+  private SlangData getSlangData(SlangData data) {
+    return new SlangData(data);
+  }
+  public SlangData getAddSlangData() {
+    return getSlangData(this.addSlangData);
+  }
+  public SlangData getDeleteSlangData() {
+    return getSlangData(this.deleteSlangData);
   }
 
   public void init() {
