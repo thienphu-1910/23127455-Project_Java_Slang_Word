527ec13735bf217692f4ca26b61c2a0fa209da57
Truong Cong Thien Phu <truongcongthienphu1910.work@gmail.com>
2025-11-15 16:31:33 +0700

    style(binarySearch): indent to the left for all statements in while loop

--- src/utils/BinarySearch.java
+++ src/utils/BinarySearch.java
@@ -8,17 +8,17 @@ public class BinarySearch {
     int low = 0, high = arr.size() -1;
 
     while (low <= high) {
-        int mid = low + (high - low) / 2;
-        
-        if (arr.get(mid).contentEquals(target))
-            return mid;
+      int mid = low + (high - low) / 2;
       
-        if (arr.get(mid).compareTo(target) < 0)
-            low = mid + 1;          
-        else
-            high = mid - 1;
-      }
+      if (arr.get(mid).contentEquals(target))
+        return mid;
+    
+      if (arr.get(mid).compareTo(target) < 0)
+        low = mid + 1;          
+      else
+        high = mid - 1;
+    }
     
     return -1;
-  }
+  } 
 }
