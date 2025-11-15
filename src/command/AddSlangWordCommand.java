def659e13c69369462afa7f1cc0e5c54a86d231c
Truong Cong Thien Phu <truongcongthienphu1910.work@gmail.com>
2025-11-15 10:59:24 +0700

    feat: add addSlangWord method, and implement execute method with the help of addSlangWord method

--- src/command/AddSlangWordCommand.java
+++ src/command/AddSlangWordCommand.java
@@ -1,4 +1,5 @@
 package command;
+
 import controller.Controller;
 
 public class AddSlangWordCommand extends Command {
@@ -6,8 +7,39 @@ public class AddSlangWordCommand extends Command {
     super(controller);
   }
 
+  // Add Condition Statement in this method
+  private void addSlangWord(String word, String definition, String mode, int position) {    
+    if (mode.contentEquals("override")) {      
+      this.controller.getGlossary().accessSlangEntry(word).addDefinition(definition, position - 1);
+    } 
+    else if (mode.contentEquals("duplicate")) {
+      position = -1;
+      this.controller.getGlossary().accessSlangEntry(word).addDefinition(definition, position);
+    } 
+    else if (mode.contentEquals("new")) {
+      this.controller.getGlossary().addSlangEntry(word, definition);
+    }
+  }
+
   @Override
   public boolean execute() {
+    String addedWord = this.controller.getAddSlangData().getAddedWordCopy();
+    String addedDefinition = this.controller.getAddSlangData().getAddedDefinitionCopy();
+
+    boolean existed = this.isExisted(addedWord);
+
+    String method = "new";
+    int position = -1;
+    // Get user requirement method here:
+
+    //
+    this.addSlangWord(addedWord, addedDefinition, method, position);
+    
     return true;
   }
 }
+
+// Add a new one
+// check the slangword in slangIdMap-> If is existed 
+// -> the SlangEntry add to the definition list or override?
+// Override -> user choose the definition to override
