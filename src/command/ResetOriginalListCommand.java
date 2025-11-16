c6c581b8e43eceb9f26466f4114b3631cf343ba1
Truong Cong Thien Phu <truongcongthienphu1910.work@gmail.com>
2025-11-16 11:05:13 +0700

    feat: add reset original list feature

--- src/command/ResetOriginalListCommand.java
+++ src/command/ResetOriginalListCommand.java
@@ -1,5 +1,7 @@
 package command;
+
 import controller.Controller;
+import database.SlangDatabase;
 
 public class ResetOriginalListCommand extends Command {
   public ResetOriginalListCommand(Controller controller) {
@@ -8,6 +10,9 @@ public class ResetOriginalListCommand extends Command {
 
   @Override
   public boolean execute() {
+    SlangDatabase db = new SlangDatabase();
+    db.fastStartup(this.controller);
+    
     return true;
   }
 }
